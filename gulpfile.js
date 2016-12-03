bd0af6b6abf552f8abfb02ec3d63ba3753a566f2
Francisco Neves <fntneves@gmail.com>
2016-12-03 12:41:06 +0000

    Start website project

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var elixir = require('laravel-elixir');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+var paths = {
+    'fontawesome': 'node_modules/font-awesome/',
+};
+
+elixir(function(mix) {
+    mix.sass('app.scss');
+
+    mix.styles([
+        'public/css/app.css',
+        paths.fontawesome + 'css/font-awesome.css',
+    ], 'public/css/app.css', './');
+
+    mix.copy([
+        paths.fontawesome + 'fonts/**'
+    ], 'public/fonts');
+});
